131e2647f5ee1459513bd961563e76fac9138945
kimsang <kimsang.mok01@gmail.com>
2023-07-19 19:06:53 +0700

    added rewards page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import { Routes, Route } from "react-router-dom"
 import Home from './Pages/HomePage/Home';
+import Rewards from './Pages/RewardsPage/Rewards';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/rewards" element={<Rewards />} />
     </Routes>
   );
 }
