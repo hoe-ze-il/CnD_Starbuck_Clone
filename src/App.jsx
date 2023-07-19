16acd6f104b5701c84ba753553d1840d474cf137
026 <anbschool0026@026s-Mac-mini.local>
2023-07-19 19:26:07 +0700

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import { Routes, Route } from "react-router-dom"
+import GiftCards from './Pages/GiftCardPage/GiftCards';
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" />
+      <Route path='/giftcards' element={<GiftCards />} />
     </Routes>
   );
 }
